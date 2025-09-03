48bb85bc1a5cb910d4142553dd199fc6c81ef4aa
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-09-03 21:32:28 +0530

    first commit

---
+++ Component/Header.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Nav from './Nav'
+
+function Header() {
+  return (
+    <div>
+        Header
+        <Nav />
+    </div>
+  )
+}
+
+export default Header
---
+++ app/MainApp.js
@@ -0,0 +1,14 @@
+
+import Header from '@/Component/Header'
+import React from 'react'
+
+function MainApp() {
+  return (
+    <div>
+        MainApp
+        <Header />
+    </div>
+  )
+}
+
+export default MainApp
